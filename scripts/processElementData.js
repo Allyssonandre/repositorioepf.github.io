c199be750a880ed4f4f0e18f7cff3c166f2df564
20172014040029 <andreallysson30@hotmail.com>
2021-07-28 20:41:28 -0300

    Site do EPF

---
+++ scripts/processElementData.js
@@ -0,0 +1,68 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_6ALvgevVEeufIPrjRP9IZgActivity"]="./../../ForAll/capabilitypatterns/resources/Implementação_48D7BB3D_48d7bb3d_Activity.jpeg"
+contentPage.processPage.imageFiles["_B0UkwevXEeufIPrjRP9IZg,_NLq3gevXEeufIPrjRP9IZg,_QRsNQevXEeufIPrjRP9IZgActivity"]="./../../ForAll/deliveryprocesses/resources/Análise_45507FBC_490b496c_Activity.jpeg"
+contentPage.processPage.imageFiles["_B0UkwevXEeufIPrjRP9IZgActivity"]="./../../ForAll/deliveryprocesses/resources/Processo análise_C27799E_c27799e_Activity.jpeg"
+contentPage.processPage.imageFiles["_B0UkwevXEeufIPrjRP9IZg,_NLq3gevXEeufIPrjRP9IZgActivity"]="./../../ForAll/deliveryprocesses/resources/Kanban_CA77016A_4c178f5c_Activity.jpeg"
+contentPage.processPage.imageFiles["_YXe2YevcEeunSZIlT7tL7QActivity"]="./../../ForAll/deliveryprocesses/resources/Processo de implementação_622529BC_622529bc_Activity.jpeg"
+contentPage.processPage.imageFiles["_6ALvgevVEeufIPrjRP9IZgActivity"]="./../../ForAll/capabilitypatterns/resources/Implementação_48D7BB3D_48d7bb3d_Activity.jpeg"
+contentPage.processPage.imageFiles["_l25-AeveEeunSZIlT7tL7QActivity"]="./../../ForAll/deliveryprocesses/resources/Processo de Testes_52BB1968_52bb1968_Activity.jpeg"
+contentPage.processPage.imageFiles["_l25-AeveEeunSZIlT7tL7Q,_oSyKkeveEeunSZIlT7tL7QActivity"]="./../../ForAll/deliveryprocesses/resources/Kanban_66E76DD6_b40b5d92_Activity.jpeg"
+contentPage.processPage.elementUrls["_dPADIOvVEeufIPrjRP9IZg"] = ["Desenvolvedores","ForAll/capabilitypatterns/Desenvolvedores_1AE8C4DF.html"];
+contentPage.processPage.elementUrls["_tADhEOvVEeufIPrjRP9IZg"] = ["Criar casos de usos","ForAll/capabilitypatterns/Criar casos de usos_56E1C605.html"];
+contentPage.processPage.elementUrls["_twbOEuvVEeufIPrjRP9IZg"] = ["Artefatos visuais","ForAll/capabilitypatterns/Artefatos visuais_7B016AA.html"];
+contentPage.processPage.elementUrls["_CBUUYOvWEeufIPrjRP9IZg"] = ["Design Técnico","ForAll/capabilitypatterns/Design Técnico_773D12C0.html"];
+contentPage.processPage.elementUrls["_dO25MOvVEeufIPrjRP9IZg"] = ["Criar visão do projeto","ForAll/capabilitypatterns/Criar visão do projeto_81200DFA.html"];
+contentPage.processPage.elementUrls["_oeqZgOvVEeufIPrjRP9IZg"] = ["Organizar informações coletadas na fase de imersão","ForAll/capabilitypatterns/organizar informações coletadas na fase de imersão_F490551.html"];
+contentPage.processPage.elementUrls["_twanAOvVEeufIPrjRP9IZg"] = ["Transformar documentos de saída da etapa de imersão em elementos visuais","ForAll/capabilitypatterns/Transformar documentos de saída da etapa de imerso em elementos _FC319A08.html"];
+contentPage.processPage.elementUrls["_oerAkOvVEeufIPrjRP9IZg"] = ["Desenvolvedores","ForAll/capabilitypatterns/Desenvolvedores_F38ED853.html"];
+contentPage.processPage.elementUrls["_oerno-vVEeufIPrjRP9IZg"] = ["Elementos lógicos do problema","ForAll/capabilitypatterns/Elementos lógicos do problema_763A139A.html"];
+contentPage.processPage.elementUrls["_GT9fMOvfEeunSZIlT7tL7Q"] = ["Testar o protótipo desenvolvido","ForAll/deliveryprocesses/Testar o protótipo desenvolvido_9947C8A3.html"];
+contentPage.processPage.elementUrls["_oernoevVEeufIPrjRP9IZg"] = ["Insights da fase de Imersão","ForAll/capabilitypatterns/Insights da fase de imersão_1B62A3D2.html"];
+contentPage.processPage.elementUrls["_Fg0yEOvfEeunSZIlT7tL7Q"] = ["Criar teste unitário","ForAll/deliveryprocesses/Criar teste unitário_FC81ED9B.html"];
+contentPage.processPage.elementUrls["_tADhEevVEeufIPrjRP9IZg"] = ["Design Técnico","ForAll/capabilitypatterns/Design Técnico_457ADA1B.html"];
+contentPage.processPage.elementUrls["_CBTtUOvWEeufIPrjRP9IZg"] = ["Criar casos de usos","ForAll/capabilitypatterns/Criar casos de usos_64F58A44.html"];
+contentPage.processPage.elementUrls["_oerAlOvVEeufIPrjRP9IZg"] = ["Análise do documento de visão","ForAll/capabilitypatterns/Análise do documento de visão_B820BA72.html"];
+contentPage.processPage.elementUrls["_NLq3gevXEeufIPrjRP9IZg"] = ["Kanban","ForAll/deliveryprocesses/Kanban_CA77016A.html"];
+contentPage.processPage.elementUrls["_Dw8e8OvWEeufIPrjRP9IZg"] = ["Desenvolvedores","ForAll/capabilitypatterns/Desenvolvedores_DFF04E62.html"];
+contentPage.processPage.elementUrls["_EfRkkOvWEeufIPrjRP9IZg"] = ["Desenvolver uma prototipação usando o conceito de MVP(Menor produto viável)","ForAll/capabilitypatterns/Desenvolver uma prototipação usando o conceito de MVP menor prod_11CBD7C9.html"];
+contentPage.processPage.elementUrls["_XUwSQevVEeufIPrjRP9IZg"] = ["Análise","ForAll/capabilitypatterns/Análise_7E7E857.html"];
+contentPage.processPage.elementUrls["_jZ5fIOvVEeufIPrjRP9IZg"] = ["Temas recorrentes em plataformas similares","ForAll/capabilitypatterns/Temas recorrentes em plataformas similares_350FAAD1.html"];
+contentPage.processPage.elementUrls["_tADhE-vVEeufIPrjRP9IZg"] = ["Coleta de dados dos insights","ForAll/capabilitypatterns/Coleta de dados dos insights_A05249E3.html"];
+contentPage.processPage.elementUrls["_CBU7cuvWEeufIPrjRP9IZg"] = ["Dados visuais da ideação","ForAll/capabilitypatterns/Dados visuais da ideação_42F677E.html"];
+contentPage.processPage.elementUrls["_dPBRQOvVEeufIPrjRP9IZg"] = ["Coleta de dados dos insights","ForAll/capabilitypatterns/Coleta de dados dos insights_666B04C4.html"];
+contentPage.processPage.elementUrls["_CBU7cOvWEeufIPrjRP9IZg"] = ["Coleta de dados dos insights","ForAll/capabilitypatterns/Coleta de dados dos insights_AF87358.html"];
+contentPage.processPage.elementUrls["_hLr7IOvVEeufIPrjRP9IZg"] = ["Participar de comunidades do público alvo","ForAll/capabilitypatterns/Participar de comunidades do público alvo_E6EEEAB9.html"];
+contentPage.processPage.elementUrls["_kemdYOvVEeufIPrjRP9IZg"] = ["Coleta de dados sobre engajamento nas plataformas similares com conteúdos do público alvo","ForAll/capabilitypatterns/Coleta de dados sobre engajamento nas plataformas similares com _9F583531.html"];
+contentPage.processPage.elementUrls["_Dw9GAOvWEeufIPrjRP9IZg"] = ["Refinamento do documento de visão revisado","ForAll/capabilitypatterns/Refinamento do documento de visão revisado requisitos do protóti_96BBA33A.html"];
+contentPage.processPage.elementUrls["_tADhFevVEeufIPrjRP9IZg"] = ["Dados visuais da ideação","ForAll/capabilitypatterns/Dados visuais da ideação_A0CBC3A.html"];
+contentPage.processPage.elementUrls["_CBU7dOvWEeufIPrjRP9IZg"] = ["Documento de visão","ForAll/capabilitypatterns/Refino no documento de visão_CF8A5577.html"];
+contentPage.processPage.elementUrls["_dPAqMOvVEeufIPrjRP9IZg"] = ["Design técnico","ForAll/capabilitypatterns/Design Técnico_54237C48.html"];
+contentPage.processPage.elementUrls["_Fg0yE-vfEeunSZIlT7tL7Q"] = ["Documento de Visão da fase de prototipação","ForAll/deliveryprocesses/Documento de visão da fase de prototipação_45F27179.html"];
+contentPage.processPage.elementUrls["_Fg0yF-vfEeunSZIlT7tL7Q"] = ["Relatório dos testes","ForAll/deliveryprocesses/Relatório dos testes_A845398.html"];
+contentPage.processPage.elementUrls["_QRsNQevXEeufIPrjRP9IZg"] = ["Análise","ForAll/deliveryprocesses/Análise_45507FBC.html"];
+contentPage.processPage.elementUrls["_f6CGAOvVEeufIPrjRP9IZg"] = ["Levantamento dos dados do público alvo similar ao do ForAll","ForAll/capabilitypatterns/Levantamento dos dados do público alvo similar ao do forall_F41F0C0C.html"];
+contentPage.processPage.elementUrls["_dPBRQuvVEeufIPrjRP9IZg"] = ["Documento de visão","ForAll/capabilitypatterns/Documento de visão_5FA1F8EA.html"];
+contentPage.processPage.elementUrls["_Dw734OvWEeufIPrjRP9IZg"] = ["Refinamento dos casos de uso","ForAll/capabilitypatterns/Refinamento dos casos de uso_BABFD7D5.html"];
+contentPage.processPage.elementUrls["_6ALvgevVEeufIPrjRP9IZg"] = ["Implementação","ForAll/capabilitypatterns/Implementação_48D7BB3D.html"];
+contentPage.processPage.elementUrls["_B0UkwevXEeufIPrjRP9IZg"] = ["Processo","ForAll/deliveryprocesses/Processo análise_C27799E.html"];
+contentPage.processPage.elementUrls["_twbOEOvVEeufIPrjRP9IZg"] = ["Desenvolvedores","ForAll/capabilitypatterns/Desenvolvedores_E792284.html"];
+contentPage.processPage.elementUrls["_oSyKkeveEeunSZIlT7tL7Q"] = ["Kanban","ForAll/deliveryprocesses/Kanban_66E76DD6.html"];
+contentPage.processPage.elementUrls["_YXe2YevcEeunSZIlT7tL7Q"] = ["Processo de implementação","ForAll/deliveryprocesses/Processo de implementação_622529BC.html"];
+contentPage.processPage.elementUrls["_l25-AeveEeunSZIlT7tL7Q"] = ["Processo de Testes","ForAll/deliveryprocesses/Processo de Testes_52BB1968.html"];
+contentPage.processPage.elementUrls["_oerAkuvVEeufIPrjRP9IZg"] = ["Design Técnico","ForAll/capabilitypatterns/Design Técnico_ECC5CC79.html"];
+contentPage.processPage.elementUrls["_tADhF-vVEeufIPrjRP9IZg"] = ["Documento de visão","ForAll/capabilitypatterns/Refino no documento de visão_64E42C02.html"];
+contentPage.processPage.elementUrls["_Fg0yEevfEeunSZIlT7tL7Q"] = ["Testador","ForAll/deliveryprocesses/Testador_EB1B01B1.html"];
+contentPage.processPage.elementUrls["_Dw9GAuvWEeufIPrjRP9IZg"] = ["MVP(Menor produto viável)","ForAll/capabilitypatterns/MVP Menor produto viável_8FF29760.html"];
+contentPage.processPage.elementUrls["_Fg0yFevfEeunSZIlT7tL7Q"] = ["Protótipo desenvolvido","ForAll/deliveryprocesses/Protótipo desenvolvido_AFACE3D0.html"];
